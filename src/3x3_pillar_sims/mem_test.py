45ae847f7060423ff9bcfa9562c205e5e1cf6728
agkgd4 <agkgd4@CENGR-DRDVWP3.umad.umsystem.edu>
2023-08-08 10:33:53 -0500

    working out kubernetes

---
+++ src/3x3_pillar_sims/mem_test.py
@@ -0,0 +1,34 @@
+import psutil
+import subprocess
+import time
+import pickle
+
+def get_memory_usage(pid):
+    process = psutil.Process(pid)
+    memory_info = process.memory_info()
+    return memory_info.rss
+
+def main():
+    program_command = ["mpirun -np 32 run_sim.py -neighbor_index 0 -resim 0 -folder_name 'n/a' -dataset 'n/a' -path_output ."]
+    process = subprocess.Popen(program_command)
+    
+    try:
+        res = []
+        while True:
+            memory_usage = get_memory_usage(process.pid) / 1e9 # MB
+            print(f"Memory usage: {memory_usage} MB")
+            time.sleep(10)
+            res.append(memory_usage)
+                   
+    except KeyboardInterrupt:
+        print(f"max = {max(res)}")
+        print(f"min = {min(res)}")
+
+        pickle.dump(open("res.pkl","wb"))
+        process.terminate()
+        process.wait()
+                
+    
+if __name__ == "__main__":
+    main()
+ 
