27a56241006b5a66ffbe52a2c7ba63c6b9d87853
agkgd4 <agkgd4@CENGR-DRDVWP3.umad.umsystem.edu>
2023-08-09 13:46:23 -0500

    abandoning mem_test.py

--- src/3x3_pillar_sims/mem_test.py
+++ src/3x3_pillar_sims/mem_test.py
@@ -10,27 +10,29 @@ def get_memory_usage(pid):
     return memory_info.rss
 
 def main():
-#    program_command = ["mpirun -np 32 run_sim.py -neighbor_index 0 -resim 0 -folder_name 'n/a' -dataset 'n/a' -path_output ."]
-#    process = subprocess.Popen(program_command)
-#    
-#    try:
-#        res = []
-#        while True:
-#            memory_usage = get_memory_usage(process.pid) / 1e9 # MB
-#            print(f"Memory usage: {memory_usage} MB")
-#            time.sleep(10)
-#            res.append(memory_usage)
-#                   
-#    except KeyboardInterrupt:
-#        print(f"max = {max(res)}")
-#        print(f"min = {min(res)}")
-#
-#        pickle.dump(open("res.pkl","wb"))
-#        process.terminate()
-#        process.wait()
-    embed()
-    neighbors = pickle.load(open("neighbors_library_allrandom.pkl", "rb"))
-    embed()                
+    program_command = ["mpirun", "-np", "32", "python3", "run_sim.py", "-neighbor_index", "0",
+                        "-resim", "0", "-folder_name", "None", "-dataset", "None",
+                        "-path_output", " ."]
+    process = subprocess.Popen(program_command)
+    
+    try:
+        i = 0
+        res = []
+        while True:
+            memory_usage = get_memory_usage(process.pid) / 1e9 # MB
+            print(f"Memory usage: {memory_usage} MB")
+            time.sleep(10)
+            res.append(memory_usage)
+                  
+    except KeyboardInterrupt:
+        print(f"max = {max(res)}")
+        print(f"min = {min(res)}")
+        
+        with open("res.pkl", "wb") as f:
+            pickle.dump(res, f)
+
+        process.terminate()
+        process.wait()
     
 if __name__ == "__main__":
     main()
